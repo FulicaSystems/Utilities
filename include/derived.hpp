d24a8847b695bdbee42268f53687f2499204439e
Jarod <jarsengkeoPro@gmail.com>
2023-03-31 23:51:18 +0200

    derived
    - perfect forwarding constructor arguments

--- include/derived.hpp
+++ include/derived.hpp
@@ -1,10 +1,23 @@
 #pragma once
 
+#include <utility>
+
 template<class TSelf, class TParent>
 class IDerived : public TParent
 {
 	friend TSelf;
 
 private:
+	// base class of this derived class
 	typedef TParent Super;
+
+	// this derived class, used to call the base class' constructor
+	typedef IDerived ctor;
+
+public:
+	IDerived() = default;
+
+	// perfect forward the constructor arguments to call the base class' constructor
+	template<typename... TType>
+	IDerived(TType&&... args) : TParent(std::forward<TType>(args)...) {}
 };
