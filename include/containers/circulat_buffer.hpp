8644408d8c9753e6d3af2f797894ed3ad94f4b73
SJarod <91637364+SJarod@users.noreply.github.com>
2024-02-01 19:47:52 +0100

    circular buffer

---
+++ include/containers/circulat_buffer.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <vector>
+#include <deque>
+
+
+template<typename TType, int size>
+class CircularBuffer
+{
+private:
+    int caret = 0;
+    std::vector<TType> buffer;
+    
+    std::deque<TType*> ptrs;
+
+
+public:
+    CircularBuffer()
+    {
+        buffer.resize(size);
+    }
+
+
+    void addData(const TType& data)
+    {
+        buffer[caret] = data;
+        ptrs.push_back(&buffer[caret]);
+        caret = (caret + 1) % size;
+    }
+
+    const TType& readData()
+    {
+        assert(!ptrs.empty());
+        auto& out = *ptrs[0];
+        ptrs.pop_front();
+        return out;
+    }
+
+    const TType* readDataPtr()
+    {
+        if (ptrs.empty())
+            return nullptr;
+
+        auto out = ptrs[0];
+        ptrs.pop_front();
+        return out;
+    }
+};
